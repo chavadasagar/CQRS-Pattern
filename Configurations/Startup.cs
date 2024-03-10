dc1d77c20330f2bffcd51cdb3910d27fe8bfe565
chavadasagr <sagarchavada80@gmail.com>
2024-03-10 15:44:48 +0530

    cqrs pattern

---
+++ Configurations/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Configuration
+{
+    public static class Startup
+    {
+        internal static ConfigureHostBuilder AddConfigurations(this ConfigureHostBuilder host)
+        {
+            host.ConfigureAppConfiguration((context, config) =>
+            {
+                const string configurationsDirectory = "Configurations";
+                var env = context.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/database.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/logger.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            });
+            return host;
+        }
+    }
+}
---
+++ Controllers/TodoController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Catalog.Todo.DTOs;
+using Application.Catalog.Todos;
+using Application.Common.Wrapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ISender _mediator;
+
+        public TodoController(ISender mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("todos")]
+        public async Task<IActionResult> GetAllTodo()
+        {
+            try
+            {
+                var todos = await _mediator.Send(new GetAllTodoQuery());
+                return Ok(await Result<List<TodoDto>>.SuccessAsync(todos,"Todos"));
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
